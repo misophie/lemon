9a10fbd26939a0045ced19242a163307a90ea797
allyoukneadisloaf <71418763+allyoukneadisloaf@users.noreply.github.com>
2024-04-07 04:19:23 -0700

    Added large lemon and friends image to landing page.

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,6 +2,7 @@ import './Landing.css';
 import { makeStyles } from '@mui/styles';
 import { Navbar } from '../components/Navbar';
 import Typography from '@mui/material/Typography';
+import lemonFriends from './lemon_landing.png';
 
 export const Landing = () => {
 
@@ -18,7 +19,12 @@ export const Landing = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <div className={classes.root} 
+    style={{ backgroundImage: `url(${lemonFriends})` ,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '98vw'}}>
       <Navbar signIn={false}/>
       <div className="mainText"> 
         <Typography variant="h6" component="div" 
@@ -27,10 +33,10 @@ export const Landing = () => {
               fontFamily: "Oxanium", 
               fontWeight: 'regular', 
               fontSize: 30 }}>
-          Strive to your better self with this goal-based newsletter while connecting with your family and friends
+          Strive to your better self with this goal-based newsletter while connecting with your family and friends!
           </Typography>
         <br></br>
-        <button type="button" className="tryBtn">Try Today</button>
+        <button type="button" className="tryBtn">Start Today</button>
       </div>
     </div>
   );
