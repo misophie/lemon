2ca92048637f623e8b3796b3f7522fd1199a98cc
zhanginc <zhanginc@students.cs.ubc.ca>
2024-04-07 04:10:16 -0700

    adds theme, fixes other bugs

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,41 +3,61 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import AppBar from '@mui/material/AppBar';
 import { PropTypes } from 'prop-types';
+import { useNavigate } from "react-router-dom";
 
 
 export const Navbar = ({signIn}) => {
+    const navigate = useNavigate();
+
+    const handleHomeClick = () => {
+        navigate("/");
+    }
+
+    const handleSignUpClick = () => {
+        navigate("/create-account");
+    }
+
     return(
         <div>
             {
             signIn ? 
             <AppBar position="static" sx={{ bgcolor: "inherit" , boxShadow: 'none'}}>
-         <Toolbar>
-          <Typography variant="h6" component="div" 
-          sx={{ flexGrow: 1 , color: "black", fontFamily: "Noto Sans", fontWeight: 'regular'}}>
-            lemon
-          </Typography>
-          </Toolbar>
-      </AppBar>
+                <Toolbar>
+                    <div onClick={handleHomeClick} style={{cursor:"pointer"}}>
+                    <Typography variant="h6" component="div" 
+                sx={{ flexGrow: 1 , color: "black", fontFamily: "Noto Sans", fontWeight: 'regular'}}>
+                    lemon
+                </Typography>
+                    </div>
+                
+                </Toolbar>
+            </AppBar>
             :
             <AppBar position="static" sx={{ bgcolor: "inherit" , boxShadow: 'none'}}>
-        <Toolbar>
-          <Typography variant="h6" component="div" 
-          sx={{ flexGrow: 1 , color: "black", fontFamily: "Noto Sans", fontWeight: 'regular'}}>
-            lemon
-          </Typography>
-          <Button color="inherit" 
-          sx={{textTransform: 'none', 
-              color: "black", 
-              fontFamily: "Oxanium",
-              fontWeight: 'light',
-              fontSize: 17 }}><Typography></Typography>About Us</Button>
-          <Button variant="outlined"
-          color="inherit" 
-          sx={{textTransform: 'none', 
-              color: "blue", 
-              fontFamily: "Oxanium",
-              fontWeight: 'light',
-              fontSize: 17 }}>Sign Up</Button>
+            <Toolbar>
+                <Typography variant="h6" component="div" 
+                sx={{ flexGrow: 1 , color: "black", fontFamily: "Noto Sans", fontWeight: 'regular'}}>
+                    lemon
+                </Typography>
+                <Button color="inherit" 
+                sx={{textTransform: 'none', 
+                    color: "black", 
+                    fontFamily: "Oxanium",
+                    fontWeight: 'light',
+                    fontSize: 17 }}><Typography>About Us</Typography>
+                </Button>
+                <Button 
+                variant="outlined"
+                color="inherit" 
+                onClick={handleSignUpClick}
+                sx={{textTransform: 'none', 
+                    color: "blue", 
+                    fontFamily: "Oxanium",
+                    fontWeight: 'light',
+                    fontSize: 17 }}>
+                Sign Up
+                    
+                </Button>
           </Toolbar>
       </AppBar>
         }
