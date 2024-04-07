41a522081aa99601f26df62120fffba32f32c129
Sophie <sophsong@student.ubc.ca>
2024-04-06 21:15:49 -0700

    Added code to access subcollections

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -14,7 +14,7 @@ export const Landing = () => {
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {
-  getFirestore, collection, getDocs, doc
+  getFirestore, collection, getDocs, doc, collectionGroup
 } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -36,12 +36,24 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore();
-const colRef = collection(db, 'groups')
+// const colRef = collection(db, 'groups')
+// const themes = collection(db, 'groups/RZZZcJxHKNf5a5HzxUIJ/themes')
+const colRef = collectionGroup(db, 'themes')
+// this is janky! it'll grab all collections named themes - not what we want
+// they do this which fixes the issue by restricting to a certain parent document
+// https://stackoverflow.com/questions/68049541/collectiongroupquery-but-limit-search-to-subcollections-under-a-particular-docum/68049847#68049847
+
+// console.log(colRef.empty)
+
+// const colRef = db.collection('groups')
+console.log(colRef)
+console.log(colRef.data)
 
 getDocs(colRef)
   .then((snapshot) => {
-    let output = []
     console.log(snapshot.docs)
+    console.log(snapshot.metadata)
+    let output = []
     snapshot.docs.forEach((doc) => {
       output.push({... doc.data()})
     })
